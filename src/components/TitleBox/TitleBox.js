a7cf851d4b7705b0a526eef2c93f60635d2a2c09
Sarah Schüßler <sts@drax.localdomain>
2021-11-22 17:11:04 +0100

    Add styling and animation of title

--- src/components/TitleBox/TitleBox.js
+++ src/components/TitleBox/TitleBox.js
@@ -1,3 +1,7 @@
+
+import styled from 'styled-components/macro';
+import {keyframes} from 'styled-components';
+
 function TitleBox () {
     return (
     <Wrapper>
@@ -8,3 +12,44 @@ function TitleBox () {
 }
 
 export default TitleBox;
+
+const romoveInLeft = keyframes`
+    from {
+        opacity: 0;
+        transform: translateX(-100px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }`
+
+const romoveInRight = keyframes`
+    from {
+        opacity: 0;
+        transform: translateX(100px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateX(0);
+    }`
+
+const Title = styled.h1`
+    color: white;
+    display: inline-block;
+    animation: ${romoveInLeft} 1s ease-out;
+`
+const SubTitle = styled.h2`
+    color: white;
+    display: inline-block;
+    animation: ${romoveInRight} 1s ease-out;
+`
+
+const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+background-color: green;
+height: 40%;
+`
