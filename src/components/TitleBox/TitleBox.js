aeb71736dc90c163f198113f685d1e72fdfdd7b4
Sarah Schüßler <sts@drax.localdomain>
2021-11-22 17:05:59 +0100

    Create Title Component

---
+++ src/components/TitleBox/TitleBox.js
@@ -0,0 +1,10 @@
+function TitleBox () {
+    return (
+    <Wrapper>
+        <Title> Hello world</Title>
+        <SubTitle>It's me!</SubTitle>
+    </Wrapper> 
+    )
+}
+
+export default TitleBox;
